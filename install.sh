4f0d1178526ecebf87389be4540ffc2bab418654
P4uLT <samter.thomas@gmail.com>
2021-11-07 17:34:05 +0100

    rework install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,43 @@
 #!/bin/sh
 
-set -e # -e: exit on error
+echo_task() {
+  printf "\033[0;34m--> %s\033[0m\n" "$*"
+}
 
-if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  chezmoi="$bin_dir/chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+error() {
+  printf "\033[0;31m%s\033[0m\n" "$*" >&2
+  exit 1
+}
+
+# -e: exit on error
+# -u: exit on unset variables
+set -eu
+
+if ! chezmoi="$(command -v chezmoi)"; then
+  bin_dir="${HOME}/.local/bin"
+  chezmoi="${bin_dir}/chezmoi"
+  echo_task "Installing chezmoi to ${chezmoi}"
+  if command -v curl >/dev/null; then
+    chezmoi_install_script="$(curl -fsSL https://git.io/chezmoi)"
+  elif command -v wget >/dev/null; then
+    chezmoi_install_script="$(wget -qO- https://git.io/chezmoi)"
   else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
+    error "To install chezmoi, you must have curl or wget."
   fi
-else
-  chezmoi=chezmoi
+  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
+  unset chezmoi_install_script bin_dir
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+# shellcheck disable=SC2312
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+
+if [ -n "${DOTFILES_ONE_SHOT-}" ]; then
+  chezmoi_extra_arg="--one-shot"
+else
+  chezmoi_extra_arg="--apply"
+fi
+
+echo_task "Running chezmoi init"
+# replace current process with chezmoi init
+exec "${chezmoi}" init --source "${script_dir}" "${chezmoi_extra_arg}"
